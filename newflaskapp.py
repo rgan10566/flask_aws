e9440cb01f7049c289622286584b3ed642d54c38
rgan10566 <rganesan@beachbody.com>
2021-01-06 18:41:08 -0800

    one more time

--- newflaskapp.py
+++ newflaskapp.py
@@ -22,7 +22,7 @@ mysql = MySQL(app)
 # Index
 @app.route('/')
 def index():
-    return render_template('./index.html')
+    return render_template('./home.html')
 
 
 # About
@@ -49,6 +49,72 @@ def assets():
         return render_template('assets.html', msg=msg)
     # Close connection
     cur.close()
+
+
+# User Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        #cur = mysql.connection.cursor()
+
+        # Execute query
+        #cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to DB
+        #mysql.connection.commit()
+
+        # Close connection
+        #cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+# User login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        #cur = mysql.connection.cursor()
+
+        # Get user by username
+        #result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        #if result > 0:
+            # Get stored hash
+            #data = cur.fetchone()
+            #password = data['password']
+
+            # Compare Passwords
+            #if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                #session['logged_in'] = True
+                #session['username'] = username
+
+                #flash('You are now logged in', 'success')
+                #return redirect(url_for('dashboard'))
+            #else:
+                #error = 'Invalid login'
+                #return render_template('login.html', error=error)
+            # Close connection
+            #cur.close()
+        #else:
+            #error = 'Username not found'
+            #return render_template('login.html', error=error)
+
+    return render_template('login.html')
 
 
 # #Single Article
