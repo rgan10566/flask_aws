e9701513bf55a8dba41e36aac84f589da6a81934
rgan10566 <rganesan@beachbody.com>
2021-01-20 14:20:52 -0800

    x

--- newflaskapp.py
+++ newflaskapp.py
@@ -81,7 +81,7 @@ def environments():
         # Create cursor
         if request.method == 'POST':
                 env = request.form['env']
-                if env is none:
+                if env is None:
                     env='null'
                 return redirect(url_for('showenvironment',env=env))
         else:
